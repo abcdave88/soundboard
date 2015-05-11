0c1f8a7992da842180f11071c1d5fb832096493e
David James <d.james88@btinternet.com>
2015-05-11 08:53:03 +0100

    soundboard

---
+++ js/main.js
@@ -0,0 +1,149 @@
+
+//ONE//
+soundManager.setup({
+  url: '/path/to/swf-files/',
+  onready: function() {
+
+    $("#one").on("click", function(){
+  var mySound = soundManager.createSound({
+  url: 'audiofiles/brucelee9.mp3'
+  });
+  mySound.play();
+
+})
+  },
+  ontimeout: function() {
+  }
+});
+//TWO//
+soundManager.setup({
+  url: '/path/to/swf-files/',
+  onready: function() {
+
+    $("#two").on("click", function(){
+  var mySound = soundManager.createSound({
+  url: 'audiofiles/brucelee1.mp3'
+  });
+  mySound.play();
+
+})
+  },
+  ontimeout: function() {
+  }
+});
+//THREE//
+soundManager.setup({
+  url: '/path/to/swf-files/',
+  onready: function() {
+
+    $("#three").on("click", function(){
+  var mySound = soundManager.createSound({
+ // id: '#two'
+  url: 'audiofiles/brucelee2.mp3'
+  });
+  mySound.play();
+
+})
+  },
+  ontimeout: function() {
+  }
+});
+//FOUR//
+soundManager.setup({
+  url: '/path/to/swf-files/',
+  onready: function() {
+
+    $("#four").on("click", function(){
+  var mySound = soundManager.createSound({
+  url: 'audiofiles/brucelee6.mp3'
+  });
+  mySound.play();
+
+})
+  },
+  ontimeout: function() {
+  }
+});
+//FIVE//
+soundManager.setup({
+  url: '/path/to/swf-files/',
+  onready: function() {
+
+    $("#five").on("click", function(){
+  var mySound = soundManager.createSound({
+  url: 'audiofiles/brucelee4.mp3'
+  });
+  mySound.play();
+
+})
+  },
+  ontimeout: function() {
+  }
+});
+//SIX//
+soundManager.setup({
+  url: '/path/to/swf-files/',
+  onready: function() {
+
+    $("#six").on("click", function(){
+  var mySound = soundManager.createSound({
+  url: 'audiofiles/brucelee5.mp3'
+  });
+  mySound.play();
+
+})
+  },
+  ontimeout: function() {
+  }
+});
+//SEVEN//
+soundManager.setup({
+  url: '/path/to/swf-files/',
+  onready: function() {
+
+    $("#seven").on("click", function(){
+  var mySound = soundManager.createSound({
+  url: 'audiofiles/brucelee3.mp3'
+  });
+  mySound.play();
+
+})
+  },
+  ontimeout: function() {
+  }
+});
+//Eight//
+soundManager.setup({
+  url: '/path/to/swf-files/',
+  onready: function() {
+
+    $("#eight").on("click", function(){
+  var mySound = soundManager.createSound({
+  url: 'audiofiles/brucelee7.mp3'
+  });
+  mySound.play();
+
+})
+  },
+  ontimeout: function() {
+  }
+});
+//NINE//
+soundManager.setup({
+  url: '/path/to/swf-files/',
+  onready: function() {
+
+    $("#nine").on("click", function(){
+  var mySound = soundManager.createSound({
+  url: 'audiofiles/brucelee8.mp3'
+  });
+  mySound.play();
+
+})
+  },
+  ontimeout: function() {
+  }
+});
+
+
+
